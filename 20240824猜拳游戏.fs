d8bdfca0d784e71fdbf7dce93bf276e8a1ede8a7
BassBaseBasic <90621406+BassBaseBasic@users.noreply.github.com>
2024-08-24 22:17:23 +0800

    Add files via upload

---
+++ 20240824猜拳游戏.fs
@@ -0,0 +1,53 @@
+﻿open System
+open System.Drawing
+
+
+let rec public guess_part = fun e ->
+    let mutable win : int = 0
+    let mutable lose : int = 0
+    let mutable draw : int = 0
+
+    let random = new Random()
+    let computer_num : int =(random.Next(1,4))
+    System.Console.WriteLine("与计算器猜拳吧，输入数字出拳：1-剪刀；2-石头；3-布；")
+    try
+        let user_num = int (Console.ReadLine())
+        if computer_num = 1 then
+            match user_num with
+            | 1 -> Console.WriteLine("你出了剪刀，电脑也出了剪刀，平局！")
+                   draw <- draw + 1
+            | 2 -> Console.WriteLine("你出了石头，电脑出了剪刀，你赢了！")
+                   win <- win + 1
+            | 3 -> Console.WriteLine("你出了布，电脑出了剪刀，你输了！")
+                   lose <- lose + 1
+         elif computer_num = 2 then
+        // 换一种Match写法
+            match user_num with
+            |_ when (user_num = 1) -> Console.WriteLine("你出了剪刀，电脑出了石头，你输了！")
+                                      lose <- lose + 1
+            |_ when (user_num = 2) -> Console.WriteLine("你出了石头，电脑也出了石头，平局！")
+                                      draw <- draw + 1
+            |_ when (user_num = 3) -> Console.WriteLine("你出了布，电脑出了石头，你赢了！")
+                                      win <- win + 1
+        elif computer_num = 3 then
+            match user_num with
+            |_ when (user_num = 1) -> Console.WriteLine("你出了剪刀，电脑出了布，你赢了！")
+                                      win <- win + 1
+            |_ when (user_num = 2) -> Console.WriteLine("你出了石头，电脑出了布，你输了！")
+                                      lose <- lose + 1
+            |_ when (user_num = 3) -> Console.WriteLine("你出了布，电脑也出了布，平局！")
+                                      draw <- draw + 1
+    with 
+        | :? FormatException -> Console.WriteLine("错误，你输入的不是数字！")
+        | :? MatchFailureException -> Console.WriteLine("错误，请输入正确的数字！")
+        | _ -> Console.WriteLine("未知错误！")
+    do guess_part()
+
+and main_part = fun e ->
+    Console.WriteLine("欢迎使用猜拳小游戏，按任意键进入游戏！")
+    Console.ReadKey()|>ignore
+    do guess_part()
+
+
+    
+main_part()
---
+++ 20240824简单的递归函数练习.fs
@@ -0,0 +1,74 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+// See the 'F# Tutorial' project for more help.
+
+let public truth : string = "Lewd Virino Meritas Lauxdon."
+
+// Define a function to construct a message to print
+
+
+open System
+open System.Windows.Forms
+open System.Drawing
+
+let rec public tiaojianpanduan = fun e ->
+    Console.WriteLine("这里是条件判断部分，请输入一个数字，与0比较！")
+    let mutable enterword : string = Console.ReadLine()
+    
+    try
+        let mutable trans : int = int enterword
+        if trans = 0 then
+            Console.WriteLine("这个数字等于0！")
+        elif trans < 0 then
+            Console.WriteLine("这个数字小于0！")
+        elif trans > 0 then
+            Console.WriteLine("这个数字大于0！")
+    with
+        | :? FormatException -> Console.WriteLine("错误，请输入数字！") 
+                                do tiaojianpanduan() 
+        | _ -> Console.WriteLine("未知错误！") 
+               do tiaojianpanduan()
+    let rec yesornoPART args = 
+        Console.WriteLine("需要继续使用本模块吗？Y/N")
+        let yes_or_no : string = Console.ReadLine()
+        let yes_or_no' = yes_or_no.ToUpper()
+        match yes_or_no' with
+            | _ when ( yes_or_no'= "Y" ) -> do tiaojianpanduan()
+            | _ when ( yes_or_no' = "N") -> Console.WriteLine("感谢使用，按任意键返回主模块。")
+                                            Console.ReadKey() |>ignore
+                                            main_part()
+            | _ -> Console.WriteLine("错误！请输入正确指令！")
+                   do yesornoPART()
+    yesornoPART()                                    
+     
+
+
+and winform_part  = fun e -> 
+    let test_form = new Windows.Forms.Form()
+    test_form.Text <- "这是一个窗口"
+    test_form.MouseClick.Add (fun e -> MessageBox.Show("这是一个弹出的窗口。%s","1")|>ignore)
+    let run' = fun e -> test_form.ShowDialog()
+    let rec yesornoPART'winform args = 
+        Console.WriteLine("需要继续使用本模块吗？Y/N")
+        let yes_or_no : string = Console.ReadLine()
+        let yes_or_no' = yes_or_no.ToUpper()
+        match yes_or_no' with
+            | "Y" -> do winform_part()
+            | _ when ( yes_or_no' = "N") -> Console.WriteLine("感谢使用，按任意键返回主模块。")
+                                            Console.ReadKey() |>ignore
+                                            main_part()
+            | _ -> Console.WriteLine("错误！请输入正确指令！")
+                   do yesornoPART'winform()
+    do run'() |>ignore
+    do yesornoPART'winform()
+
+and main_part = fun e ->
+    Console.WriteLine("输入数字来进入相应功能：1-条件判断；2-窗口创建；")
+    let choice = Console.ReadLine()
+    match choice with
+    | "1" -> tiaojianpanduan()
+    | "2" -> winform_part()|>ignore
+    | _ ->  Console.WriteLine("无效输入，请重新输入！")
+            main_part()
+
+[<EntryPoint>]
+main_part()
